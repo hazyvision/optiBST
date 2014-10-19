80b1351c7fbad89dd70ac4877ffa26de9923f310
hazyvision <adrienC562@gmail.com>
2014-10-18 20:40:03 -0700

    Initial update

--- memFunc.py
+++ memFunc.py
@@ -1,3 +1,13 @@
-#In Beta
-#Inbeta
-#from ubuntu
+
+def memoryFunc(a,i,j):
+	if a[i][j] is None: 
+		memoryFunc(a,i,j) #produce result
+		a[i][j] = result
+	else:   result = a[i][j]
+	return result
+
+i = 0
+j = 2
+arr = [[None for x in xrange(5)] for x in xrange(5)]
+memoryFunc(arr,i,j)
+
